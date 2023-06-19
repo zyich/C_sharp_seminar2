d5e065a07ebcfc3968f00c2152ca98dc8114122f
zyich <zuilinho@yandex.ru>
2023-06-19 23:18:55 +0200

    task 4

---
+++ task4_Kratnoe_23_7_inOneMoment/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+7 и 23.
+
+14 -> нет
+46 -> нет
+161 -> да
+*/
+
+bool IsMultiplicity(int num)
+{
+    
+    if(num%7==0 && num%23==0)
+    {
+       return true;
+    }
+    else
+    {
+        return false;   
+    }
+
+    
+}
+Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = IsMultiplicity(num);
+Console.WriteLine(result);
